0b1563e37f98ad455a994e4e1c64522ea42a46f2
john050481 <john0504@yandex.ru>
2020-05-20 22:40:22 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,160 +10,162 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fas);
 
 function MenuSeparator({item}) {
-  return <li className={ "menu-separator" + (item.className ? ` ${item.className}` : "") } />;
+    return <li className={ "menu-separator" + (item.className ? ` ${item.className}` : "") } />;
 }
 function MenuItem({item, createSubmenu = false}) {
-  return (
-    <li className={ "menu-item" + (createSubmenu ? " submenu" : "") + (item.className ? ` ${item.className}` : "") } data-data={item.data}>
-      <button type="button" className="menu-btn">
-        <FontAwesomeIcon className="fa" icon={ item.icon ? item.icon : 'circle' } />
-        <span className="menu-text">{item["title"]}</span>
-      </button>
-      {createSubmenu && createSubmenu(item["submenu"], true)}
-    </li>
-  );
+    return (
+        <li className={ "menu-item" + (createSubmenu ? " submenu" : "") + (item.className ? ` ${item.className}` : "") } data-data={item.data}>
+            <button type="button" className="menu-btn">
+                <FontAwesomeIcon className="fa" icon={ item.icon ? item.icon : 'circle' } />
+                <span className="menu-text">{item["title"]}</span>
+            </button>
+            {createSubmenu && createSubmenu(item["submenu"], true)}
+        </li>
+    );
 }
 
 export default function ContextMenu(props) {
 
-  let menuElem = useRef(null);
-  useOnClickOutside(menuElem, props.hideMenu);
+    let menuElem = useRef(null);
+    useOnClickOutside(menuElem, props.hideMenu);
 
-  useLayoutEffect( () => {
-      console.log('useEffect', menuElem.current, props);
+    useLayoutEffect( () => {
+        console.log('useEffect', menuElem.current, props);
 
-      if (!menuElem.current) return;
+        if (!menuElem.current) return;
 
-      const widthWindow = document.documentElement.clientWidth;
-      const heightWindow = document.documentElement.clientHeight;
+        const widthWindow = document.documentElement.clientWidth;
+        const heightWindow = document.documentElement.clientHeight;
 
-      function checkOutWindow(checkedElement, isRootMenu = false) {
-          if (!isRootMenu) {
-              checkedElement.classList.remove('menuRight');
-              checkedElement.classList.add('menuLeft');
-              checkedElement.classList.remove('menuBottom');
-              checkedElement.classList.add('menuTop');
-          }
+        function checkOutWindow(checkedElement, isRootMenu = false) {
+            if (!isRootMenu) {
+                checkedElement.classList.remove('menuRight');
+                checkedElement.classList.add('menuLeft');
+                checkedElement.classList.remove('menuBottom');
+                checkedElement.classList.add('menuTop');
+            }
 
-          const coordsCheckedElement = checkedElement.getBoundingClientRect();
+            const coordsCheckedElement = checkedElement.getBoundingClientRect();
 
-          if (widthWindow < coordsCheckedElement.x + coordsCheckedElement.width) {
-              if (isRootMenu) checkedElement.style.left = widthWindow - coordsCheckedElement.width + 'px';
+            if (widthWindow < coordsCheckedElement.x + coordsCheckedElement.width) {
+                if (isRootMenu)
+                    checkedElement.style.left = widthWindow - coordsCheckedElement.width + 'px';
 
-              if (!isRootMenu) {
-                  checkedElement.classList.add('menuRight');
-                  checkedElement.classList.remove('menuLeft');
-              }
-          };
-          if (heightWindow < coordsCheckedElement.y + coordsCheckedElement.height) {
-              if (isRootMenu) checkedElement.style.top = heightWindow - coordsCheckedElement.height + 'px';
+                if (!isRootMenu) {
+                    checkedElement.classList.add('menuRight');
+                    checkedElement.classList.remove('menuLeft');
+                }
+            };
+            if (heightWindow < coordsCheckedElement.y + coordsCheckedElement.height) {
+                if (isRootMenu)
+                    checkedElement.style.top = heightWindow - coordsCheckedElement.height + 'px';
 
-              if (!isRootMenu) {
-                  checkedElement.classList.add('menuBottom');
-                  checkedElement.classList.remove('menuTop');
-              }
-          };
-      };
+                if (!isRootMenu) {
+                    checkedElement.classList.add('menuBottom');
+                    checkedElement.classList.remove('menuTop');
+                }
+            };
+        };
 
-      // correction of the position of the root menu, if it is outside the window
-      checkOutWindow(menuElem.current, true);
+        // correction of the position of the root menu, if it is outside the window
+        checkOutWindow(menuElem.current, true);
 
-      // correction of the position of the submenus, if it is outside the window
-      let submenus = Array.from(document.querySelectorAll('menu')).slice(1);
-      submenus.forEach( submenu => {
-          checkOutWindow(submenu, false);
-      } );
-  });
+        // correction of the position of the submenus, if it is outside the window
+        let submenus = Array.from(document.querySelectorAll('menu')).slice(1);
+        submenus.forEach( submenu => {
+            checkOutWindow(submenu, false);
+        } );
+    });
 
-  function onClickMenu(e) {
-    let parentLiElem = e.target.closest("li.menu-item:not(.submenu)");
-    if (parentLiElem) {
-        props.callbackOnClickMenu(parentLiElem.dataset.data, parentLiElem);
-        props.hideMenu();
+    function onClickMenu(e) {
+        let parentLiElem = e.target.closest("li.menu-item:not(.submenu)");
+        if (parentLiElem) {
+            props.callbackOnClickMenu(parentLiElem.dataset.data, parentLiElem);
+            props.hideMenu();
+        };
     };
-  };
 
-  function createMenu(arrMenuItem, submenu = false) {
+    function createMenu(arrMenuItem, submenu = false) {
+        return (
+            <menu
+                ref = {submenu ? null : menuElem}
+                className = {submenu ? "menu" : "menu show-menu"}
+                style = {submenu ? null : {left: props.pageXY[0], top: props.pageXY[1]}}
+                onClick = {submenu ? null : e => onClickMenu(e)}
+            >
+                {arrMenuItem.map((item, i) => {
+                    if (item["type"] === "item")
+                        return <MenuItem key={i} item={item} />
+
+                    if (item["type"] === "separator")
+                        return <MenuSeparator key={i} item={item} />
+
+                    if (item["type"] === "submenu")
+                        return <MenuItem key={i} item={item} createSubmenu={createMenu} />
+
+                    return <div key={i}>{item["type"]}</div>
+                })}
+            </menu>
+        );
+    }
+
     return (
-      <menu
-          ref = {submenu ? null : menuElem}
-          className = {submenu ? "menu" : "menu show-menu"}
-          style = {submenu ? null : {left: props.pageXY[0], top: props.pageXY[1]}}
-          onClick = {submenu ? null : e => onClickMenu(e)}
-      >
-        {arrMenuItem.map((item, i) => {
-          if (item["type"] === "item")
-              return <MenuItem key={i} item={item} />
-
-          if (item["type"] === "separator")
-              return <MenuSeparator key={i} item={item} />
-
-          if (item["type"] === "submenu")
-              return <MenuItem key={i} item={item} createSubmenu={createMenu} />
-
-          return <div key={i}>{item["type"]}</div>
-        })}
-      </menu>
-    );
-  }
-
-  return (
-      props.visible
-          ? <div className='react-contextmenu'>{createMenu(props.items)}</div>
-          : null
-  )
+        props.visible
+            ? <div className='react-contextmenu'>{createMenu(props.items)}</div>
+            : null
+    )
 }
 
 ContextMenu.propTypes = {
-  visible: PropTypes.bool.isRequired,
-  hideMenu: PropTypes.func.isRequired,
-  pageXY: PropTypes.array.isRequired,
-  items: PropTypes.array.isRequired,
-  callbackOnClickMenu: PropTypes.func.isRequired
+    visible: PropTypes.bool.isRequired,
+    hideMenu: PropTypes.func.isRequired,
+    pageXY: PropTypes.array.isRequired,
+    items: PropTypes.array.isRequired,
+    callbackOnClickMenu: PropTypes.func.isRequired
 };
 MenuSeparator.propTypes = {
-  item: PropTypes.object.isRequired
+    item: PropTypes.object.isRequired
 };
 MenuItem.propTypes = {
-  item: PropTypes.object.isRequired,
-  createSubmenu: PropTypes.func
+    item: PropTypes.object.isRequired,
+    createSubmenu: PropTypes.func
 };
 
 // for example
 ContextMenu.defaultProps = {
-  visible: true,
-  hideMenu: () => {},
-  pageXY: [0, 0],
-  items: [
-    { type: "item", title: "defItem1", data: "defdata1", icon: "check-square" },
-    {
-      type: "item",
-      title: "defItem2",
-      data: "defdata2",
-      className: "disabled"
-    },
-    { type: "separator" },
-    {
-      type: "submenu",
-      title: "defSubMenu1",
-      submenu: [
-        { type: "item", title: "defItemSubMenu1", data: "defdataSubMenu1" },
+    visible: true,
+    hideMenu: () => {},
+    pageXY: [0, 0],
+    items: [
+        { type: "item", title: "defItem1", data: "defdata1", icon: "check-square" },
         {
-          type: "submenu",
-          title: "defSubMenu2",
-          submenu: [
-            {
-              type: "item",
-              title: "defItemSubSubMenu1",
-              data: "defdataSubSubMenu1"
-            }
-          ]
+            type: "item",
+            title: "defItem2",
+            data: "defdata2",
+            className: "disabled"
         },
-        { type: "item", title: "defItemSubMenu2", data: "defdataSubMenu2" }
-      ]
+        { type: "separator" },
+        {
+            type: "submenu",
+            title: "defSubMenu1",
+            submenu: [
+                { type: "item", title: "defItemSubMenu1", data: "defdataSubMenu1" },
+                {
+                    type: "submenu",
+                    title: "defSubMenu2",
+                    submenu: [
+                        {
+                            type: "item",
+                            title: "defItemSubSubMenu1",
+                            data: "defdataSubSubMenu1"
+                        }
+                    ]
+                },
+                { type: "item", title: "defItemSubMenu2", data: "defdataSubMenu2" }
+            ]
+        }
+    ],
+    callbackOnClickMenu: (data, parentLiElem) => {
+        console.log("default callbackOnClickMenu = ", data, parentLiElem);
     }
-  ],
-  callbackOnClickMenu: (data, parentLiElem) => {
-    console.log("default callbackOnClickMenu = ", data, parentLiElem);
-  }
 };
