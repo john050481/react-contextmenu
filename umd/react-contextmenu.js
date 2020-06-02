ea44e59a336f37633702eb9fe4c960ab477fb347
john050481 <john0504@yandex.ru>
2020-06-02 21:58:07 +0300

    fix

--- umd/react-contextmenu.js
+++ umd/react-contextmenu.js
@@ -1,5 +1,5 @@
 /*!
- * @john0504/react-contextmenu v2.0.0 - https://github.com/john050481/react-contextmenu#readme
+ * @john0504/react-contextmenu v2.0.1 - https://github.com/john050481/react-contextmenu#readme
  * MIT Licensed
  */
 (function webpackUniversalModuleDefinition(root, factory) {
